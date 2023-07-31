575b13828d55858656344f06fac55b6a4a13c510
Tatiana <tati14953@gmail.com>
2023-07-30 22:33:30 -0300

    Adapto calculadora para calculo adaptado CANCELACIÓN DE APORTES PREVISIONALES

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 function calcular() {
-    const remuneracionInput = document.getElementById("remuneracion");
-    const smvmInput = document.getElementById("smvm");
+    const remuneracionInput = document.getElementById("remuneracion");   
     const mesesInput = document.getElementById("meses");
     const cuotasSelect = document.getElementById("cuotas");
 
     const meses = parseInt(mesesInput.value);
     if (meses < 1 || meses > 360 || isNaN(meses)) {
-      alert("Por favor ingrese un número válido de meses (entre 1 y 360)");
+      alert("Por favor ingrese un número válido de meses (entre 1 - 360)");
       return;
     }
-
-    const valorUPP = (remuneracionInput.value * 0.29).toFixed(2);
-    const montoMaximoCuota = (smvmInput.value * 0.3).toFixed(2);
-
+    
+    const valorUPP = (remuneracionInput.value * 0.29).toFixed(2);  
     const cuotas = parseInt(cuotasSelect.value);
-    const totalPagar = (valorUPP * meses).toFixed(2);
+    const totalPagar = (valorUPP * mesesInput.value).toFixed(2);
     const cuotaMensual = (totalPagar / cuotas).toFixed(2);
-    const cuotaMensualUpp = (cuotaMensual / valorUPP).toFixed(2);
+    
+    const formatter = new Intl.NumberFormat("es-AR");
+    const valorUPPFormatted = formatter.format(valorUPP);
+    const totalPagarFormatted = formatter.format(totalPagar);
+    const cuotaMensualFormatted = formatter.format(cuotaMensual);
 
     const resultado = document.getElementById("resultado");
-    resultado.innerHTML = `Valor UPP: $${valorUPP}<br>Monto máximo mensual por cuota: $${montoMaximoCuota}<br>Deuda a regularizar: $${totalPagar}<br> Meses a regularizar ${meses}<br>Monto a pagar mensual: $${cuotaMensual}<br>Cantidad de UPP por mes: ${cuotaMensualUpp}`;
+    
+    resultado.innerHTML = `Valor UPP: $${valorUPPFormatted}<br> Deuda total: $${totalPagarFormatted}<br> Meses a regularizar ${meses}<br>Monto a pagar mensual: $${cuotaMensualFormatted}`;
 }
 
 function toggleDarkMode() {
